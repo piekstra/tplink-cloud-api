edec516013637e17376f1e6cffebb9bfeef635c1
piekstra <piekstra@users.noreply.github.com>
2020-10-19 23:56:38 -0700

    Fixing bug in power toggling (#9)
    This was having issues for non-HS300 devices.
    
    This also fixes a regression introduced when sys_info was set up as actual classes
    
    Co-authored-by: piekstra <piekstra.dev@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 with open('README.md', 'r', encoding='utf-8') as readme:
     long_description = readme.read()
