31c7fa7cb0801695e72c2207988c175f0c16b622
piekstra <piekstra@users.noreply.github.com>
2020-12-08 20:50:47 -0800

    Add HS300 None checks (#11)
    Found a bug breaking initialization because one of the HS300 devices was mis-reporting. Since we "prefetch" all devices, it broke all requests using the device manager.
    
    Co-authored-by: piekstra <piekstra.dev@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 with open('README.md', 'r', encoding='utf-8') as readme:
     long_description = readme.read()
