0a448cbe8f9b11b6386ca6e234331897cada6765
piekstra <piekstra.dev@gmail.com>
2021-02-27 22:00:14 -0800

    Bumping release to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 with open('README.md', 'r', encoding='utf-8') as readme:
     long_description = readme.read()
