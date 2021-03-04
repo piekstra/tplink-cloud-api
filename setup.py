4d20d1af45a1c82c4c0a0e33482c6f7a695534cc
piekstra <piekstra.dev@gmail.com>
2021-03-03 23:02:48 -0800

    Bump Python version minimum to 3.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'requests==2.22.0'
     ],
     url='https://github.com/piekstra/tplink-cloud-api',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     zip_safe=False,
     license='GPL-3',
     classifiers=[
