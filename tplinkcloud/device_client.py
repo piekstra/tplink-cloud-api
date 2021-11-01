5960d4559a4dae7afa188109e46df6525aaeccb8
Shayne Hartford <shaybox@shaybox.com>
2021-11-01 07:58:21 -0400

    Update device_client.py

--- tplinkcloud/device_client.py
+++ tplinkcloud/device_client.py
@@ -1,8 +1,8 @@
-import asyncio
 import aiohttp
 import json
 import uuid
 
+from .asyncio import asyncio_run
 from .api_response import TPLinkApiResponse
 
 
@@ -53,7 +53,7 @@ class TPLinkDeviceClient:
                     raise Exception(str(response.status) + ': ' + response.reason)
     
     def _request_post(self, body):
-        return asyncio.run(self._request_post_async(body))
+        return asyncio_run(self._request_post_async, body)
 
     async def pass_through_request_async(self, device_id, request_data):
         body = {
@@ -70,4 +70,4 @@ class TPLinkDeviceClient:
         return None
 
     def pass_through_request(self, device_id, request_data):
-        return asyncio.run(self.pass_through_request_async(device_id, request_data))
+        return asyncio_run(self.pass_through_request_async, device_id, request_data)
