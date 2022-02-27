babc4410c4230a418c6392961daaed1bb7298199
piekstra <piekstra@users.noreply.github.com>
2022-02-26 18:37:57 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '4.1.1'
+__version__ = '4.1.2'
 
 with open('README.md', 'r', encoding='utf-8') as readme:
     long_description = readme.read()
