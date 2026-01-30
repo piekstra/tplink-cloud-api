b06e991da9fdb6d6aa48fb60bd3dc3fcc9face14
piekstra <piekstra@users.noreply.github.com>
2026-01-30 18:11:48 -0500

    Add pytest-asyncio configuration
    - Add pytest.ini with asyncio_mode = auto
    - Remove deprecated event_loop fixture from conftest.py
      (pytest-asyncio now handles this automatically)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import asyncio
 
 try:
     from .local_env_vars import ENV_VARS
@@ -20,10 +19,3 @@ def tests_setup_and_teardown():
     # Will be executed after the last test
     os.environ.clear()
     os.environ.update(original_environ)
-
-
-@pytest.fixture(scope='module')
-def event_loop(request):
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
