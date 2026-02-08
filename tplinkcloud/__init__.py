7b1b7a4e99133eabe1af10fca7775e2426f750dc
piekstra <piekstra@users.noreply.github.com>
2026-02-08 00:46:29 -0500

    #94, #95: Modernize codebase and update project config (#110)
    - Replace setup.py + pytest.ini with pyproject.toml
    - Remove Windows ProactorBasePipeTransport hack from __init__.py
      (fixed in aiohttp years ago)
    - Replace 14-branch if/elif device dispatch with dictionary lookup
    - Install package in CI via pip install -e .
    
    [#94] [#95]

--- tplinkcloud/__init__.py
+++ tplinkcloud/__init__.py
@@ -19,25 +19,3 @@ __all__ = [
     'TPLinkMFARequiredError',
     'TPLinkTokenExpiredError',
 ]
-
-# Windows OS-specific HACK to silence exception thrown on event loop being closed
-# as part of the asyncio library's proactor
-# Hack sourced from an issue on the aiohttp library:
-#   https://github.com/aio-libs/aiohttp/issues/4324#issuecomment-733884349
-# This assumes you have the asyncio library installed
-import platform
-if platform.system() == 'Windows':
-    from functools import wraps
-    from asyncio.proactor_events import _ProactorBasePipeTransport
-
-    def silence_event_loop_closed(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            try:
-                return func(self, *args, **kwargs)
-            except RuntimeError as e:
-                if str(e) != 'Event loop is closed':
-                    raise
-        return wrapper
-
-    _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
